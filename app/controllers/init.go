204206dfa36127c8fcb60f912f8b2d8cc07254f1
musale <martinmshale@gmail.com>
2017-02-07 12:07:08 +0300

    Init and create initial DB

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -2,38 +2,55 @@ package controllers
 
 import "github.com/revel/revel"
 
-func init(){
-    revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
-    revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
-    revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
+func init() {
+	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
+	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
 }
 
 func getParamString(param string, defaultValue string) string {
-    p, found := revel.Config.String(param)
-    if !found {
-        if defaultValue == "" {
-            revel.ERROR.Fatal("Cound not find parameter: " + param)
-        } else {
-            return defaultValue
-        }
-    }
-    return p
+	p, found := revel.Config.String(param)
+	if !found {
+		if defaultValue == "" {
+			revel.ERROR.Fatal("Cound not find parameter: " + param)
+		} else {
+			return defaultValue
+		}
+	}
+	return p
 }
 
 func getConnectionString() string {
-    host := getParamString("db.host", "")
-    port := getParamString("db.port", "3306")
-    user := getParamString("db.user", "")
-    pass := getParamString("db.password", "")
-    dbname := getParamString("db.name", "goblog")
-    protocol := getParamString("db.protocol", "tcp")
-    dbargs := getParamString("dbargs", " ")
+	host := getParamString("db.host", "")
+	port := getParamString("db.port", "3306")
+	user := getParamString("db.user", "")
+	pass := getParamString("db.password", "")
+	dbname := getParamString("db.name", "goblog")
+	protocol := getParamString("db.protocol", "tcp")
+	dbargs := getParamString("dbargs", " ")
 
-    if strings.Trim(dbargs, " ") != "" {
-        dbargs = "?" + dbargs
-    } else {
-        dbargs = ""
-    }
-    return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s",
-        user, pass, protocol, host, port, dbname, dbargs)
+	if strings.Trim(dbargs, " ") != "" {
+		dbargs = "?" + dbargs
+	} else {
+		dbargs = ""
+	}
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s",
+		user, pass, protocol, host, port, dbname, dbargs)
+}
+
+var InitDb func() = func() {
+	connectionString := getConnectionString()
+	if db, err := sql.Open("mysql", connectionString); err != nil {
+		revel.ERROR.Fatal(err)
+	} else {
+		Dbm = &gorp.DbMap{
+			Db:      db,
+			Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	}
+	// Defines the table for use by GORP
+	// This is a function we will create soon.
+	defineBidItemTable(Dbm)
+	if err := Dbm.CreateTablesIfNotExists(); err != nil {
+		revel.ERROR.Fatal(err)
+	}
 }
