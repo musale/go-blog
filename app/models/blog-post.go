ed5bb7e119ba0e3169f26ec3fb6b7895148bd952
musale <martinmshale@gmail.com>
2017-02-07 15:04:59 +0300

    Move validator into the same file

--- app/models/blog-post.go
+++ app/models/blog-post.go
@@ -1,5 +1,8 @@
 package models
-import time
+import (
+    "github.com/revel/revel"
+    "time"
+)
 
 type BlogPost struct {
     Id                 int64        `db:"id" json:"id"`
@@ -7,4 +10,20 @@ type BlogPost struct {
     Category           string       `db:"category" json:"category"`
     DateOfPublishing   time.Time    `db:"date_of_publishing" json:"date_of_publishing"`
     Author             string       `db:"author" json:"author"`
+}
+
+
+func (b *BlogPost) Validate(v *revel.Validation) {
+
+    v.Check(b.Author,
+        revel.ValidRequired())
+
+    v.Check(b.DateOfPublishing,
+        revel.ValidRequired())
+
+    v.Check(b.Title,
+        revel.ValidRequired())
+
+    v.Check(b.Id,
+        revel.ValidRequired())
 }
