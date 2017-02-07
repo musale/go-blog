7b078945aab69c12ac4d70789095da908c5fa451
musale <martinmshale@gmail.com>
2017-02-07 11:48:01 +0300

    Set up the validation for the BlogPost

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+    "github.com/revel/revel"
+    "regexp"
+)
 
 type App struct {
 	*revel.Controller
@@ -8,4 +11,19 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (b *BlogPost) Validate(v *revel.Validation) {
+
+    v.Check(b.Author,
+        revel.ValidRequired())
+
+    v.Check(b.DateOfPublishing,
+        revel.ValidRequired())
+
+    v.Check(b.Title,
+        revel.ValidRequired())
+
+    v.Check(b.Id,
+        revel.ValidRequired())
 }
