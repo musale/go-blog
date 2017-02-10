70dbb5481b5d8d668d27c8bf9db8405ca5c87563
musale <martinmshale@gmail.com>
2017-02-10 17:51:00 +0300

    Added the BlogPost body

--- app/models/blog-post.go
+++ app/models/blog-post.go
@@ -1,29 +1,36 @@
 package models
+
 import (
-    "github.com/revel/revel"
-    "time"
+	"time"
+
+	"github.com/revel/revel"
 )
 
+// BlogPost struct
 type BlogPost struct {
-    Id                 int64        `db:"id" json:"id"`
-    Title              string       `db:"title" json:"title"`
-    Category           string       `db:"category" json:"category"`
-    DateOfPublishing   time.Time    `db:"date_of_publishing" json:"date_of_publishing"`
-    Author             string       `db:"author" json:"author"`
+	ID               int64     `db:"id" json:"id"`
+	Title            string    `db:"title" json:"title"`
+	Body             string    `db:"body" json:"body"`
+	Category         string    `db:"category" json:"category"`
+	DateOfPublishing time.Time `db:"date_of_publishing" json:"date_of_publishing"`
+	Author           string    `db:"author" json:"author"`
 }
 
-
+// Validate a BlogPost
 func (b *BlogPost) Validate(v *revel.Validation) {
 
-    v.Check(b.Author,
-        revel.ValidRequired())
+	v.Check(b.Author,
+		revel.ValidRequired())
 
-    v.Check(b.DateOfPublishing,
-        revel.ValidRequired())
+	v.Check(b.Body,
+		revel.ValidRequired())
 
-    v.Check(b.Title,
-        revel.ValidRequired())
+	v.Check(b.DateOfPublishing,
+		revel.ValidRequired())
 
-    v.Check(b.Id,
-        revel.ValidRequired())
+	v.Check(b.Title,
+		revel.ValidRequired())
+
+	v.Check(b.ID,
+		revel.ValidRequired())
 }
